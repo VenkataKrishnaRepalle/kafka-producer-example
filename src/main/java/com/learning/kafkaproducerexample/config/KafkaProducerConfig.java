74f226d127d5a52b7c862227560816ed7da4c211
repalle venkata krishna <rvkrishna13052001@gmail.com>
2024-10-23 17:57:05 +0530

    Kafka producer implemented and new topic created

---
+++ src/main/java/com/learning/kafkaproducerexample/config/KafkaProducerConfig.java
@@ -0,0 +1,14 @@
+package com.learning.kafkaproducerexample.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class KafkaProducerConfig {
+
+    @Bean
+    public NewTopic createTopic() {
+        return new NewTopic("kafka-impl", 5, (short) 1);
+    }
+}
